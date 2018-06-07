af3c7e67de8de23d1c03eb62d10b03ccc8049aaa
zhangleiming <zhangleiming@corp-ci.com>
2018-06-07 16:22:04 +0800

    添加获取问卷list接口

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,9 +1,13 @@
 import * as Net from './net.js'
 const Base64 = require('js-base64').Base64
 export function getLoginInfo (callback) {
-  Net.request('GET', '/wyeth/user/getLoginInfo', {back_url: Base64.encode(window.location.pathname + window.location.hash)}, callback)
+  Net.request('GET', '/wyeth/user/getLoginInfo', { back_url: Base64.encode(window.location.pathname + window.location.hash) }, callback)
 }
 
 export function postAnswer (data, callback) {
   Net.request('POST', '/survey/submit', data, callback)
+}
+
+export function getAnswer (data, callback) {
+  Net.request('GET', '/survey/list', data, callback)
 }
